2dd78b2b21587b29f88f9602b72c3b925166601b
dreamlx <dreamlinx@gmail.com>
2011-07-17 18:37:24 +0800

    modified:   app/controllers/projects_controller.rb 	modified:   app/helpers/projects_helper.rb 	modified:   app/views/personalcharges/index.html.erb 	modified:   config/menu.yml

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -47,7 +47,7 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @booking = Booking.new
-    check_sum_hours
+    #check_sum_hours
     
     respond_to do |format|
       format.html # show.rhtml
