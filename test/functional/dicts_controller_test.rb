756e31f4d6f28241de0d550398c08ad018f85348
ken-wong <wangkun025@gmail.com>
2014-10-28 19:41:56 +0800

    Remove test files

--- test/functional/dicts_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class DictsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
--- test/unit/helpers/dicts_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class DictsHelperTest < ActionView::TestCase
-end
